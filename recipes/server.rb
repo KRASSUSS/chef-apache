61501582fe26a3d1b125dab7b4447d8dca2e5ae1
KRASSUSS <staskrasuski@gmail.com>
2021-09-27 12:16:03 +0200

    v0.3.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -14,14 +14,10 @@ service 'apache2' do
   action [:disable, :start]
 end
 
-file '/var/www/html/index.html' do
-  content "<h1> Hello, this is working, start the weekend! </h1>
-  <h3>
-  Hostname: #{node['hostname']}<br>
-  IP Adress: #{node['ipaddress']}<br> 
-  CPU: #{node['cpu']['0']['mhz']}<br>
-  Memory: #{node['memory']['total']}<br>
-  </h3>
-  "
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  variables(
+    :name => 'KRASSUSS'
+  )
 end
 
