530a3855bf57c49120c9aedfcc6de163f2274393
KRASSUSS <staskrasuski@gmail.com>
2021-09-29 10:10:00 +0200

    v 0.5.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,17 +8,49 @@
 
 package 'tree'
 
-package 'apache2'
-
-service 'apache2' do
-  action [:disable, :start]
+package 'apache2' do
+  case node['platform']
+  when 'centos', 'redhat', 'fedora', 'suse'
+    package_name 'httpd'
+  when 'debian', 'ubuntu'
+    package_name 'apache2'
+  when 'arch'
+    package_name 'apache'
+  end
+  action :install
 end
+
+
+if platform?('centos', 'redhat', 'fedora', 'suse')
+	service 'httpd' do
+	  action [:enable, :start]
+	end
+end
+if platform?('debian', 'ubuntu')
+	service 'apache2' do
+	  action [:enable, :start]
+	end
+end
+if platform?('arch')
+	service 'apache' do
+	  action [:enable, :start]
+	end
+end
+
 
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   variables(
-    :name => 'KRASSUSS'
+   :name => 'KRASSUSS'
   )
-  notifies :restart, 'service[apache2]', :immediately
+  if platform?('centos', 'redhat', 'fedora', 'suse')
+	notifies :restart, 'service[httpd]', :immediately
+  end
+  if platform?('ubuntu', 'debian')
+	notifies :restart, 'service[apache2]', :immediately
+  end
+  if platform?('arch')
+	notifies :restart, 'service[apache]', :immediately
+  end
 end
 
